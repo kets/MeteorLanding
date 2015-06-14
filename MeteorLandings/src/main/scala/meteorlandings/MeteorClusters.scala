142d3f4a63cc736368f4b8509bbb48b643621577
Ketaki Borkar <kborkar@us.ibm.com>
2015-06-13 19:33:44 -0500

    created vectors of lat,lon and implemented k-means

--- MeteorLandings/src/main/scala/meteorlandings/MeteorClusters.scala
+++ MeteorLandings/src/main/scala/meteorlandings/MeteorClusters.scala
@@ -1,6 +1,6 @@
 package meteorlandings
 
-import org.apache.spark.mllib.linalg.Vector
+import org.apache.spark.mllib.linalg._
 import org.apache.spark.mllib.feature.HashingTF
 import org.apache.spark.mllib.clustering.KMeans
 import org.elasticsearch.spark._
@@ -36,30 +36,56 @@ object MeteorClusters {
     //val query = "{\"query\": {\"filtered\" : {\"query\" : {\"match_all\" : {}}}}}"
     println("Using query "+query)
     jobConf.set("es.query", query)
+    
+    val esRDD = sc.esRDD("test2/nasa2", query)
       
     //val esRDD = sc.esRDD("test2/nasa2", query)
     
      // Read from ES using inputformat from org.elasticsearch.hadoop;
-     // note, that key [Text] specifies the document id (_id) and
-     // value [MapWritable] the document as a field -> value map
+     // note, that key [Object] specifies the document id (_id) and
+     // value [MapWritable] the document as a field -> value map (location -> "34.45,23.45"
     //
     val currentResults = sc.hadoopRDD(jobConf, classOf[EsInputFormat[Object, MapWritable]], classOf[Object], classOf[MapWritable])
     println("currentResults: "+ currentResults)
     val meteors = currentResults.map{ case (key, value) => mapWritableToInput(value) }
     //val meteors = sqlCtx.esRDD("test/nasa", query)
     
+      
+    
     println("count: " + currentResults.count())
-    // Extract the location
-    val location = meteors.flatMap{meteor =>
+    
+    // Extract the location into a map
+    val location = meteors.flatMap({meteor =>
       meteor.getOrElse("location", "").split(" ")
-    }
+    })
     
-   
+    val fields = new Array[String](97)
     
- 
+    val vectors = meteors.map(meteor => toVector(meteor.getOrElse("location", "").split(","), fields))
+    
+    // Cluster the data into two classes using KMeans
+    val numClusters = 2
+    val numIterations = 20
+    val model = KMeans.train(vectors, numClusters, numIterations)
+      
+    
+    println(vectors.countByValue())
 
-  
+    
+    //val vectors = Vectors.dense(location)
+     
   }
+  
+  private def toVector(data:Array[String], fields:Array[String]):Vector = {
+
+    val lat = data(0).toDouble
+    val lon = data(1).toDouble
+
+    Vectors.dense(Array(lat,lon))
+
+  }
+  
+  
   
   def mapWritableToInput(in: MapWritable): Map[String, String] = {
     in.map{case (k, v) => (k.toString, v.toString)}.toMap
---
+++ MeteorLandings/src/main/scala/meteorlandings/NaiveBayesSample.scala
@@ -0,0 +1,5 @@
+package meteorlandings
+
+object NaiveBayesSample {
+
+}
